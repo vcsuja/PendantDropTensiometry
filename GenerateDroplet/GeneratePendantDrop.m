16da50e5d9b7ef15ae259239b96ecc23bd3d6508
Vinny Chandran Suja <vinny@stanford.edu>
2019-05-25 15:29:58 -0700

    Initial implementation of the generateDropImages function

--- GenerateDroplet/GeneratePendantDrop.m
+++ GenerateDroplet/GeneratePendantDrop.m
@@ -38,8 +38,41 @@ classdef GeneratePendantDrop < handle
                 dropProfiles{1,i} =xTheory;
                 dropProfiles{2,i} =yTheory;
                 
+                %Update the properties of the class:
+                obj.Bo  = Bo;
+                obj.dropProfiles = dropProfiles;
+                
             end
         end
+        
+        function dropImages = generateDropImages(obj,imgSize,imgAugment)
+            % Generates dropImages for training a machine learning model
+            % Inputs: imgSize -  The size of the output image (default: 64x64)
+            %       : imgAugment - The number of translational and rotational augments (default: 0)
+            
+            % Check if dropProfiles are available
+            if (isempty(obj.dropProfiles))
+                error('Run generateDropProfiles() to generate the dropProfiles before running this function');
+            end
+            
+            % Handle optional arguments:
+            if (nargin<3)
+                imgAugment = 0;
+            end
+            if (nargin<2)
+                imgSize = 64;
+            end
+           
+            % Preallocate memory for the arrays:
+              dropImages = zeros(length(obj.Bo)*(imgAugment+1),imgSize,imgSize);
+              
+            for i =1:length(obj.Bo)
+                dropImages(i,:,:)=obj.profile2Image(obj.dropProfiles{1,i},obj.dropProfiles{2,i},imgSize);
+            end
+            
+            
+        end
+        
     end
     
     
@@ -56,6 +89,23 @@ classdef GeneratePendantDrop < handle
             ydot(3) = 2 - Bo*y(2) - sin(y(3))/y(1);
         end
         ydot = ydot';
+        end
+        
+        function im = profile2Image(~,xProfile,yProfile,nsize)
+        % This function generates an image corresponding to the supplied
+        % profile
+        
+        figure('Units', 'pixels','Position',[500,500,300,300],'Color','white','visible','off')
+        plot(xProfile,yProfile,'Linewidth',1.4,'Color',[0,0,1])
+        ylim([-max(yProfile)*0.25 1.25*max(yProfile)]);
+        xlim(1.1*[-max(xProfile) max(xProfile)]);
+        axis equal
+           
+        set(gca, 'Units', 'pixels', 'Position', [10, 10, nsize, nsize]);
+        fr = getframe(gca,[0,0,nsize,nsize]);
+        close(gcf);
+        im = frame2im(fr);
+        im = im2double(im(round(end-nsize)/2:round(end-nsize)/2+nsize-1,round(end-nsize)/2:round(end-nsize)/2+nsize-1,2));           
         end
     end
     
